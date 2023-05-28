6451ffc835c4d3e1d82c8cbe391305b028b76adb
YusukeIwaki <iwaki@i3-systems.com>
2023-05-28 16:29:07 +0900

    Allow download mdm.mobileconfig multiple times.

--- util/use_push_certificate.rb
+++ util/use_push_certificate.rb
@@ -19,6 +19,27 @@ push_certificate = OpenSSL::X509::Certificate.new(File.read(filepath))
 p12 = OpenSSL::PKCS12.create(ENV['PUSH_CERTIFICATE_PASSWORD'], 'oreore-mdm-push', push_key, push_certificate)
 server_key = OpenSSL::PKey::RSA.new(File.read('util/server.key'))
 
+# create device certificate
+# This certificate should be different for each device,
+# however no way do distinguish the device on the access of GET /mdm.mobileconfig by Safari.
+# So, we use the same certificate for all devices.
+device_cert = OpenSSL::X509::Certificate.new
+device_cert.not_before = Time.now
+device_cert.not_after = Time.now + 60 * 60 * 24 * 365
+device_cert.public_key = server_key.public_key
+device_cert.serial = 0
+device_cert.issuer = OpenSSL::X509::Name.new([["CN", "oreore-mdm"]])
+device_cert.subject = OpenSSL::X509::Name.new([["CN", "oreore-mdm-device"]])
+device_cert.sign(server_key, OpenSSL::Digest::SHA256.new)
+device_pkcs12 = OpenSSL::PKCS12.create(
+  '!oreore-mdm',
+  'oreore-mdm-device',
+  server_key,
+  device_cert)
+
+File.open('util/device.p12', 'wb') { |f| f.write(device_pkcs12.to_der) }
+
 puts "set an environment variable:"
 puts "  PUSH_CERTIFICATE_BASE64=#{Base64.strict_encode64(p12.to_der)}"
 puts "  SERVER_PRIVATE_KEY_BASE64=#{Base64.strict_encode64(server_key.to_pem)}"
+puts "  DEVICE_CERTIFICATE_BASE64=#{Base64.strict_encode64(device_pkcs12.to_der)}"
--- views/mdm.mobileconfig.rb
+++ views/mdm.mobileconfig.rb
@@ -1,22 +1,7 @@
-server_key = OpenSSL::PKey::RSA.new(Base64.strict_decode64(ENV['SERVER_PRIVATE_KEY_BASE64']))
-
-# create device certificate
-device_key = OpenSSL::PKey::RSA.generate(2048)
-issuer = OpenSSL::X509::Name.new([["CN", "oreore-mdm"]])
-
-device_cert = OpenSSL::X509::Certificate.new
-device_cert.not_before = Time.now
-device_cert.not_after = Time.now + 60 * 60 * 24 * 365
-device_cert.public_key = device_key.public_key
-device_cert.serial = 0
-device_cert.issuer = OpenSSL::X509::Name.new([["CN", "oreore-mdm"]])
-device_cert.subject = OpenSSL::X509::Name.new([["CN", "oreore-mdm-device"]])
-device_cert.sign(server_key, OpenSSL::Digest::SHA256.new)
-device_pkcs12 = OpenSSL::PKCS12.create(
+device_pkcs12 = OpenSSL::PKCS12.new(
+  Base64.strict_decode64(ENV['DEVICE_CERTIFICATE_BASE64']),
   '!oreore-mdm',
-  'oreore-mdm-device',
-  device_key,
-  device_cert)
+)
 
 identity_cerificate = MobileConfig::SecurityPkcs12.new(
   pkcs12: device_pkcs12,
@@ -27,6 +12,7 @@ identity_cerificate = MobileConfig::SecurityPkcs12.new(
 topic = PushCertificate.from_env.topic
 
 payload = MobileConfig.new(
+  uuid: ENV['MDM_MOBILECONFIG_PAYLOAD_UUID'],
   display_name: 'Oreore MDM configuration',
   contents: [
     identity_cerificate,
