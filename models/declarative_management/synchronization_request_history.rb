d82b3a745a0f6bdb79b27424061eaac5c37b7a44
YusukeIwaki <iwaki@i3-systems.com>
2023-07-10 23:55:29 +0900

    fix re-model bug

--- models/declarative_management/synchronization_request_history.rb
+++ models/declarative_management/synchronization_request_history.rb
@@ -4,18 +4,18 @@ module DeclarativeManagement
     attribute :request_payload, :json
     attribute :response_payload, :json
 
-    def self.log_response(udid, endpoint, data, response_json)
+    def self.log_response(device, endpoint, data, response_json)
       create!(
-        udid: udid,
+        mdm_device: device,
         endpoint: endpoint,
         request_payload: data,
         response_payload: response_json,
       )
     end
 
-    def self.log_404(udid, endpoint, data)
+    def self.log_404(device, endpoint, data)
       create!(
-        udid: udid,
+        mdm_device: device,
         endpoint: endpoint,
         request_payload: data,
       )
