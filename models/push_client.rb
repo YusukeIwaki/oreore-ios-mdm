51292e5ab9e830f70f831c91b78d28142e768ca4
YusukeIwaki <iwaki@i3-systems.com>
2023-08-07 10:18:31 +0900

    Setup GitHub Actions

--- models/push_client.rb
+++ models/push_client.rb
@@ -1,19 +1,12 @@
 class PushClient
-  # @param [PushCertificate] push_certificate
-  def initialize
-    pkcs12, password = PushCertificate.from_env._top_secrets
-    @apnotic_connection = Apnotic::Connection.new(
-      cert_path: StringIO.new(pkcs12.to_der),
-      cert_pass: password,
-    )
-  end
-
-  class Response
-    def initialize(apnotic_response)
-      @status = apnotic_response.status
-      @bodu = body == '' ? {} : apnotic_response.body
+  class Response < Struct.new(:status, :apns_id, :body)
+    def self.from(apnotic_response)
+      new(
+        apnotic_response.status.to_i,
+        apnotic_response.headers["apns-id"],
+        apnotic_response.body.presence || {},
+      )
     end
-    attr_reader :status, :body
   end
 
   # @param [MdmPushEndpoint|ByodPushEndpoint] push_endpoint
@@ -22,6 +15,16 @@ class PushClient
     notification = Apnotic::MdmNotification.new(
                     token: push_endpoint.token,
                     push_magic: push_endpoint.push_magic)
-    @apnotic_connection.push(notification)
+    Response.from(apnotic_connection.push(notification))
+  end
+
+  private def apnotic_connection
+    return @apnotic_connection if @apnotic_connection
+
+    pkcs12, password = PushCertificate.from_env._top_secrets
+    @apnotic_connection = Apnotic::Connection.new(
+      cert_path: StringIO.new(pkcs12.to_der),
+      cert_pass: password,
+    )
   end
 end
