f0571a9ef22bfe5cabc07fccdada7bc9fed91f6a
YusukeIwaki <iwaki@i3-systems.com>
2023-12-11 02:25:42 +0900

    Enable update installing MDM mobileconfig

--- views/mdm.mobileconfig.rb
+++ views/mdm.mobileconfig.rb
@@ -17,6 +17,7 @@ payload = MobileConfig.new(
   contents: [
     identity_cerificate,
     MobileConfig::Mdm.new(
+      uuid: ENV['MDM_MOBILECONFIG_PAYLOAD_UUID'],
       topic: topic,
       identity_cerificate: identity_cerificate,
     ),
